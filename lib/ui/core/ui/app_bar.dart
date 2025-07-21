1c8bbdac2ce8d7269db5c10e1611c35139ef6bc2
Ivan <astanin.i.a@gmail.com>
2025-07-21 13:43:02 +0300

    removed animation from appbar routes

--- lib/ui/core/ui/app_bar.dart
+++ lib/ui/core/ui/app_bar.dart
@@ -1,4 +1,11 @@
 import 'package:anime_app/l10n/app_localizations.dart';
+import 'package:anime_app/router/no_animation_route.dart';
+import 'package:anime_app/ui/anime_list/view/anime_list_screen.dart';
+import 'package:anime_app/ui/collections/view/view.dart';
+import 'package:anime_app/ui/favourites/favourites.dart';
+import 'package:anime_app/ui/history/view/history_screen.dart';
+import 'package:anime_app/ui/search/view/view.dart';
+import 'package:anime_app/ui/splash/splash.dart';
 import 'package:flutter/material.dart';
 
 class AnimeBar extends StatelessWidget {
@@ -13,6 +20,23 @@ class AnimeBar extends StatelessWidget {
     Color activeColor = Theme.of(context).colorScheme.secondary;
     Color inactiveColor = Colors.white;
 
+    Widget routeBuilder(BuildContext context, String route) {
+      switch (route) {
+        case '/anime/list':
+          return AnimeListScreen();
+        case '/favourites':
+          return FavouritesScreen();
+        case '/collections':
+          return CollectionsScreen();
+        case '/anime/search':
+          return SearchScreen();
+        case '/history':
+          return HistoryScreen();
+        default:
+          return SplashScreen();
+      }
+    }
+
     Widget navItem(String route, IconData icon, String label) {
       final isActive = currentRoute == route;
       final color = isActive ? activeColor : inactiveColor;
@@ -23,7 +47,13 @@ class AnimeBar extends StatelessWidget {
           GestureDetector(
             onTap: () {
               if (!isActive && !isBlocked) {
-                Navigator.pushReplacementNamed(context, route);
+                Navigator.pushReplacement(
+                  context,
+                  NoAnimationPageRoute(
+                    builder: (context) => routeBuilder(context, route),
+                    settings: RouteSettings(name: route),
+                  ),
+                );
               }
             },
             child: Icon(icon, color: color, size: 30),
